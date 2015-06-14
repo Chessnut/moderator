1b44d46cb6dc7c5ec8a2fd30ae40389a81c86f54
Nookyava <jcw21396@gmail.com>
2015-06-14 15:23:09 -0400

    Revert "Added player rank in ( ) next to name"
    This reverts commit 3565db2946649909b910269477cf3d840e868710.

--- lua/moderator/derma/menus/cl_players.lua
+++ lua/moderator/derma/menus/cl_players.lua
@@ -262,10 +262,12 @@ local CATEGORY = {}
 			surface.SetDrawColor(0, 0, 0, 60)
 			surface.DrawOutlinedRect(0, 0, w, h)
 			
-			local rank = moderator.GetGroup(self.player)
-			local rankinfo = (moderator.GetGroupTable(rank))
-			surface.SetDrawColor(rankinfo.color)
-			surface.DrawRect(w - 7, 1, 6, h - 2)
+			if (self.playerSet) then
+				local teamColor = team.GetColor(self.player:Team())
+					
+				surface.SetDrawColor(teamColor)
+				surface.DrawRect(w - 7, 1, 6, h - 2)
+			end
 		end
 		
 		function PANEL:SetPlayer(client)
@@ -307,9 +309,7 @@ local CATEGORY = {}
 			end
 			self.avatar.click:SetToolTip("Click to view this "..(client.SteamName and client:SteamName() or client:Name()).."'s Steam profile.")
 			
-			local rank = moderator.GetGroup(self.player)
-			local rankinfo = (moderator.GetGroupTable(rank))
-			self.name:SetText(client:Name() .." ("..rankinfo.name..")")
+			self.name:SetText(client:Name())
 			self.name:SizeToContents()
 
 			self.icon:SetImage("icon16/"..moderator.GetGroupIcon(client)..".png")
