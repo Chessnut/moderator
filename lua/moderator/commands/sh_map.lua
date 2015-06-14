a4a8cfb2dab94f684921d7c0163f4ef1f73573b8
Nookyava <jcw21396@gmail.com>
2015-06-14 15:07:37 -0400

    Added Map Change Command

---
+++ lua/moderator/commands/sh_map.lua
@@ -0,0 +1,25 @@
+local COMMAND = {}
+	COMMAND.name = "Map"
+	COMMAND.icon = "map"
+	COMMAND.hidden = true
+	COMMAND.noTarget = true
+	COMMAND.usage = "<string map>"
+	COMMAND.example = "!map map - Set the current map."
+
+	function COMMAND:OnRun(client, arguments)
+		local map = tostring(arguments[1])
+		local currentmap = game.GetMap()
+		
+		if map && file.Exists("maps/"..map..".bsp", "GAME") then
+			moderator.NotifyAction(client, nil, "has changed the map to "..map)
+			
+			timer.Simple(0.5, function()
+				RunConsoleCommand("changelevel", map)
+			end)
+		elseif map == currentmap then
+			client:ChatPrint("You can't change the map to the map we're already on!")
+		else
+			client:ChatPrint("That is not a valid map!")
+		end
+	end
+moderator.commands.map = COMMAND
