91b518976fa70f7bce87943c434f565746e90ad3
Jacob <jcwilks21396@gmail.com>
2015-10-31 02:41:06 -0400

    Fixed current map check
    A previous logic error, the command was not checking to see if the map
    was currently what the players were on. Fixed to check it.

--- lua/moderator/commands/sh_map.lua
+++ lua/moderator/commands/sh_map.lua
@@ -1,3 +1,9 @@
+--[[
+	Moderator Map Command
+	Last Updated: 10/31/2015
+	Purpose: A command that allows the client to change the map to an existing one on the server.
+]]
+
 local COMMAND = {}
 	COMMAND.name = "Map"
 	COMMAND.icon = "map"
@@ -10,15 +16,15 @@ local COMMAND = {}
 		local map = arguments[1]
 		local currentmap = game.GetMap()
 		
-		if map && file.Exists("maps/"..map..".bsp", "GAME") then
+		if map && file.Exists("maps/"..map..".bsp", "GAME") && map != currentmap then -- Need to see if the file exists naturally, and we also need to check to see if the current map is the one they're trying to change it to.
 			moderator.NotifyAction(client, nil, "has changed the map to "..map)
 			
-			timer.Simple(0.5, function()
+			timer.Simple(0.5, function() -- We do a timer just to add a slight delay.
 				RunConsoleCommand("changelevel", map)
 			end)
 		elseif map == currentmap then
 			client:ChatPrint("You can't change the map to the map we're already on!")
-		else
+		else -- If all else fails, the map just doesn't exist.
 			client:ChatPrint("That is not a valid map!")
 		end
 	end
